341d7bf2a0823717445f65e256c2f28bee3b9a79
tekibo <tekibohelp@gmail.com>
2025-09-15 21:12:15 +0530

    fixing issues with imports

--- android/src/main/kotlin/com/trkibo/google_sign_in_native/GoogleSignInNativePlugin.kt
+++ android/src/main/kotlin/com/trkibo/google_sign_in_native/GoogleSignInNativePlugin.kt
@@ -9,6 +9,8 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
